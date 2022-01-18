2ebd5454c372930254e6b1cf6cfa4a24c84e5eaa
wsxfvhu <krmiv77@gmail.com>
2022-01-18 19:12:05 +0300

    Добавьте файлы проекта.

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Func<double, double> CF = CircleLength;
+            Console.WriteLine($"Длина окружности радиусом {r} равна {CF?.Invoke(r)}");
+            Console.WriteLine();
+            CF = AreaCircle;
+            Console.WriteLine($"Площадь круга радиусом {r} равна {CF?.Invoke(r)}");
+            Console.WriteLine();
+            CF = VolumeBall;
+            Console.WriteLine($"Объем шара радиусом {r} равен {CF?.Invoke(r)}");
+            Console.ReadKey();
+        }
+        public static double CircleLength(double r)
+        {
+            double d = 2 * Math.PI * r;
+            return d;
+        }
+        public static double AreaCircle(double r)
+        {
+            double s = Math.PI * r * r;
+            return s;
+        }
+        public static double VolumeBall(double r)
+        {
+            double v = 1.3333333333333 * Math.PI * r * r * r;
+            return v;
+        }
+
+    }
+}
